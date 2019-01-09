85384b02b00341824dfff6586f2ca97aa1cd9fca
VilhelmNielsen <seb.vilhelm@gmail.com>
2019-01-09 16:20:32 +0100

    mocking apollo-hooks

--- frontend/src/lib/testingUtils.js
+++ frontend/src/lib/testingUtils.js
@@ -1,13 +1,32 @@
 import casual from 'casual'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { MockLink } from 'apollo-link-mock'
 
 casual.seed(666)
 
 export const fakeUser = overrides => ({
   id: 'abc123',
-  name: casual.name,
+  name: casual.full_name,
   email: casual.email,
   items: [],
   bookings: [],
   pendingBookings: [],
   ...overrides,
 })
+
+export const fakeCategory = overrides => ({
+  id: 'cat345',
+  title: casual.title,
+  slug: casual.word,
+  description: casual.description,
+  items: [],
+  ...overrides,
+})
+
+export function createClient(mocks) {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new MockLink(mocks),
+  })
+}
