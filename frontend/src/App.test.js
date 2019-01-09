a8158aab18a035e103b66c24b59ca4cdac9ad2e9
VilhelmNielsen <seb.vilhelm@gmail.com>
2019-01-09 16:20:44 +0100

    app test init, but no point in doing it

---
+++ frontend/src/App.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { ApolloProvider } from 'react-apollo-hooks'
+import { render, wait } from 'react-testing-library'
+import { createClient, fakeCategory } from './lib/testingUtils'
+import App from './App'
+import { QUERY_CURRENT_USER } from './components/User'
+import { QUERY_CATEGORIES } from './components/Categories'
+
+const categoryMock = {
+  request: { query: QUERY_CATEGORIES },
+  result: { data: { categories: [fakeCategory()] } },
+}
+
+const unauthenticatedMock = [
+  {
+    request: {
+      query: QUERY_CURRENT_USER,
+    },
+    result: {
+      data: { me: null },
+    },
+  },
+  categoryMock,
+]
+
+describe('<App />', () => {
+  xit('renders and matches snapshot', async () => {
+    const client = createClient(unauthenticatedMock)
+    const { getByText, debug } = render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    )
+    await wait()
+    debug()
+  })
+})
