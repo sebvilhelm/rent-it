288784f13943e4adb58f91d13188d37b25249399
VilhelmNielsen <seb.vilhelm@gmail.com>
2019-01-10 15:54:39 +0100

    unit test for getUserId

---
+++ backend/src/__tests__/getUserId.test.js
@@ -0,0 +1,29 @@
+const getUserId = require('../utils/getUserId')
+const jwt = require('jsonwebtoken')
+
+global.process.env.APP_SECRET = 'test'
+
+test('can get user id from request', async () => {
+  const id = 'abc123'
+  const token = jwt.sign({ userId: id }, process.env.APP_SECRET)
+  const userId = await getUserId({
+    request: {
+      cookies: {
+        token,
+      },
+    },
+  })
+  expect(userId).toBe(id)
+})
+
+test('it fails when there is no userId', async () => {
+  try {
+    await getUserId({
+      request: {
+        cookies: {},
+      },
+    })
+  } catch (error) {
+    expect(error).toEqual(Error('You must be logged in to do that'))
+  }
+})
