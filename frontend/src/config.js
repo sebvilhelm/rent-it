73cbffb8805cc40725116d52efa015f0adaddf32
VilhelmNielsen <seb.vilhelm@gmail.com>
2019-01-15 15:00:00 +0100

    move endpoint to .env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,3 @@
-export const endpoint = 'http://localhost:4000'
-
 export const siteMeta = {
   title: 'Rent it',
 }
--- frontend/src/lib/createApolloClient.js
+++ frontend/src/lib/createApolloClient.js
@@ -1,12 +1,8 @@
 import ApolloClient from 'apollo-boost'
-import { endpoint } from '../config'
 
 function createApolloClient() {
   return new ApolloClient({
-    uri:
-      process.env.NODE_ENV === 'development'
-        ? endpoint
-        : process.env.REACT_APP_GRAPHQL_ENDPOINT,
+    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
     credentials: 'include',
   })
 }
